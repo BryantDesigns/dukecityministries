56e72a9181f8a9356febb124b25a4029131e3e88
Justin Lynch <jtown.80@gmail.com>
2024-04-09 19:23:54 -0500

    Fixed Packages component to receive props of tiers. Also set tier description to scroll instead of showing all of the description text.
    Co-authored-by: tyguyb@gmail.com <tyguyb@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@ import About from './components/About.vue'
 import Packages from './components/Packages.vue'
 import Policies from './components/Policies.vue'
 import Reviews from './components/Reviews.vue'
+import Title from './components/Title.vue'
+import tiers from '../src/assets/data/tiers'
+
+console.log(tiers)
 </script>
 
 <template>
-  <Packages />
+  <Title />
+  <Packages :tiersArray=tiers.tiers />
   <Policies />
   <About />
   <Reviews />
